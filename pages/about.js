b25d377414aa92008a1426cfef41ab389dae48d5
Mehmed Erkekli <mehmederkekli@gmail.com>
2021-06-15 22:51:58 +0300

    Create about.js

---
+++ pages/about.js
@@ -0,0 +1,20 @@
+import Head from 'next/head'
+import Header from '@components/Header'
+import Footer from '@components/Footer'
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>konsoll start!</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <Header title="Welcome to the start of konsoll!!" />
+      </main>
+
+      <Footer />
+    </div>
+  )
+}
