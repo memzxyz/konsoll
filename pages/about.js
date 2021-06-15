7066ac5bf0feed5bcfd2e7aa00854aa134d7e614
Mehmed Erkekli <mehmederkekli@gmail.com>
2021-06-15 22:55:18 +0300

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -11,7 +11,7 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to the start of konsoll!!" />
+        <Header title="Here's a bit about us" />
       </main>
 
       <Footer />
